d2ed4624879347ee816ce77093071d04d015cc80
Murtaza Yurtsev <murtaza@studiorebels.nl>
2025-10-04 17:01:35 +0200

    minor width & spacing improvements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="max-w-5xl mx-auto my-6">
+      <router-view />
+    </div>
   </div>
 </template>
 
