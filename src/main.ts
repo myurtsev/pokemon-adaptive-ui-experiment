e7312153fa6594eb8a42e667b52710ae8d8893c4
Murtaza Yurtsev <murtaza@studiorebels.nl>
2025-10-04 16:53:41 +0200

    Fix router configuration for production build

--- src/main.ts
+++ src/main.ts
@@ -9,26 +9,8 @@ const app = createApp(App)
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: import.meta.hot ? [] : routes,
+  routes: routes,
 })
-
-if (import.meta.hot) {
-  const removeRoutes = []
-
-  for (const route of routes) {
-    removeRoutes.push(router.addRoute(route))
-  }
-}
-if (import.meta.hot) {
-  import.meta.hot?.accept('./routes.ts', ({ routes }) => {
-    for (const removeRoute of removeRoutes) removeRoute()
-    removeRoutes = []
-    for (const route of routes) {
-      removeRoutes.push(router.addRoute(route))
-    }
-    router.replace('')
-  })
-}
 
 app.use(createPinia())
 app.use(router)
